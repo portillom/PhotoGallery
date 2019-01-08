27e6380f0700db8e6c2fb497226d21c8b1ff9ed0
USER <portillom@g.cofc.edu>
2019-01-07 18:10:40 -0800

    Initial commit

---
+++ app/src/main/java/com/michaelportillo/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,38 @@
+package com.michaelportillo.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ViewSwitcher;
+
+/**
+ * Created by USER on 1/7/19.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance(){
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
+    }
+}
