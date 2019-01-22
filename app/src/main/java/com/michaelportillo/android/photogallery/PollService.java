49c98580f5038e139704c7d46f752b9500b1fb39
USER <portillom@g.cofc.edu>
2019-01-22 13:41:12 -0800

    Checking for background network availability

--- app/src/main/java/com/michaelportillo/android/photogallery/PollService.java
+++ app/src/main/java/com/michaelportillo/android/photogallery/PollService.java
@@ -3,6 +3,7 @@ package com.michaelportillo.android.photogallery;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /**
@@ -22,6 +23,18 @@ public class PollService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
         Log.i(TAG, "Received an intent: " + intent);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
     }
 }
